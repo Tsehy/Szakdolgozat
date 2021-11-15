c15ead34e88d24e5f893ba61f6932a76079bd11e
Tsehy <67957571+Tsehy@users.noreply.github.com>
2021-11-15 12:05:42 +0100

    test code for the plane calculation

---
+++ source_code/plane_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+p1 = np.array([1, 0, 0])
+p2 = np.array([0, 1, 0])
+p3 = np.array([0, 0, 1])
+one = np.array([[1], [1], [1], [1]])
+
+p = np.array([0, 0, 0])
+
+A0 = np.matrix([p1, p2, p3])
+
+A = np.vstack([p, A0])
+
+A = np.append(A, one, axis=1)
+
+print(A)
+print(np.linalg.det(A))
